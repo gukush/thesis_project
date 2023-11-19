ace9dae22d4c6b4effa4b3cf4c84afd8078cb16a
Cimin123 <151384049+Cimin123@users.noreply.github.com>
2023-11-19 22:31:17 +0100

    dodanie dodatkowych pól w UI

--- scripts/streamlit.py
+++ scripts/streamlit.py
@@ -1,12 +1,46 @@
 import streamlit as st
 import fitz
+
+# File uploader
 uploaded_file = st.file_uploader("Test upload")
+
+# Column layout
+col1, col2, col3 = st.columns(3)
+
+# First column: Types of analysis
+with col1:
+    extractive_summary = st.checkbox("Extractive Summary")
+    basic_analysis = st.checkbox("Basic Analysis")
+    advanced_analysis = st.checkbox("Advanced Analysis")
+    num_sentences = st.number_input("Number of sentences in summary", min_value=1, value=1, step=1)
+
+# Second column: User roles
+with col2:
+    analyst = st.checkbox("Analyst")
+    investor = st.checkbox("Investor")
+    shareholder = st.checkbox("Shareholder")
+
+# Third column: Report options
+with col3:
+    whole_report = st.checkbox("Whole Report")
+    # Pages between
+    start_page = st.number_input("Start from page (0-indexed)", min_value=0, value=0, step=1, key='start_page')
+    end_page = st.number_input("End at page (0-indexed)", min_value=0, value=0, step=1, key='end_page')
+
+# Processing the uploaded file
 if uploaded_file is not None:
     bytes_data = uploaded_file.getvalue()
     doc = fitz.Document(stream=bytes_data)
-    for page in doc:
-        st.write(len(page.get_text("text")))
+
+    # Initialize an empty string to store summary or analysis
+    output_text = ""
+
+    # Logic to process the document based on user input
+    # This part of the code should be adapted based on the specific functionalities
+    # of your analysis and summary functions
+    # ...
+
+    # Display the output
+    st.write(output_text)
 
 st.write("Hello World2!")
-st.write("Tartaaaraaara")
-st.write("Muszę się skupić na inżynierce, studia i tak mam")

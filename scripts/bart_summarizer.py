00b55bad898f7b6701a21cf37061d2a46c5f3b59
gukush <s184339@student.pg.edu.pl>
2023-11-20 21:41:28 +0100

    BART notes

--- scripts/bart_summarizer.py
+++ scripts/bart_summarizer.py
@@ -1,10 +1,13 @@
 from transformers import pipeline
 import torch 
 
+# Possible future direction - training the BART model to some text that we need 
+
+
 summarizer = pipeline("summarization", model="facebook/bart-base")
 
 def abstractive(text):
-    text_ = text[:1024] # temporary workaround because model was crashing
+    text_ = text[:1024] # temporary workaround because model was crashing TODO: fix the tokenization
     summary = summarizer(text_)
     return summary
 
